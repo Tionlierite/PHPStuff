6f9933912d4b4face78462e0e0e11be56c2718ff
Tionlierite <Tionlierite@gmail.com>
2024-04-10 03:10:21 +0300

    [feat] Added dockerfile and PHP scripts
    - Dockerfile now creates image and take care of all the work with Apache
    - Docker-compose simply builds container from created image
    - Created 2 PHP scripts, which mounted directly inside Apache to specially created folders for virtual hosts

--- Task 1/src/nsk.time.test.php
+++ Task 1/src/nsk.time.test.php
@@ -1,8 +1,5 @@
 <?php
 
-$dateLosAngeles = new DateTime("now", new DateTimeZone('America/Los_Angeles'));
 $dateNovosibirsk = new DateTime("now", new DateTimeZone('Asia/Novosibirsk'));
-
-echo '<p>' . 'Time in Los Angeles right now: ' . $dateLosAngeles->format('H:i:s'). '<p>';
 
 echo '<p>' . 'Time in Novosibirsk right now: ' . $dateNovosibirsk->format('H:i:s'). '<p>';
