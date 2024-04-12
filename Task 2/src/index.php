8790bfb932431949e331aa5a2457aabcdab88df9
Tionlierite <Tionlierite@gmail.com>
2024-04-12 18:24:23 +0300

    [impr] Minor code style improvements

--- Task 2/src/index.php
+++ Task 2/src/index.php
@@ -1,31 +1,30 @@
 <?php
-$path = $_SERVER['REQUEST_URI'];
+$path = $_SERVER["REQUEST_URI"];
 
-if ($path !== '/favicon.ico') {
-    $pdo = new PDO('pgsql:host=postgres;dbname=visitsDB', 'tion', 'dotwrk');
-
+if ($path !== "/favicon.ico") {
     switch ($path) {
-        case '/l-a':
-            include './time-test/l-a.time.test.php';
+        case "/l-a":
+            include "./time-test/l-a.time.test.php";
             break;
-        case '/nsk':
-            include './time-test/nsk.time.test.php';
+        case "/nsk":
+            include "./time-test/nsk.time.test.php";
             break;
         default:
-            include './time-test/l-a.time.test.php';
+            include "./time-test/l-a.time.test.php";
             break;
     }
 
-    $currentDateTime = date('Y-m-d H:i:s');
+    $currentDateTime = date("Y-m-d H:i:s");
 
+    $pdo = new PDO("pgsql:host=postgres;dbname=visitsDB", "tion", "dotwrk");
     $stmt = $pdo->prepare("INSERT INTO visits (domain, visited_at) VALUES (?, ?)");
-    $stmt->execute([$_SERVER['HTTP_HOST'], $currentDateTime]);
+    $stmt->execute([$_SERVER["HTTP_HOST"], $currentDateTime]);
 
     $stmt = $pdo->query("SELECT domain, visited_at FROM visits ORDER BY id DESC LIMIT 10");
     $visits = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo '<h2>Previous Visits:</h2>';
+    echo "<h2>Previous Visits:</h2>";
     foreach ($visits as $visit) {
-        echo '[' . $visit['domain'] . '] - ' . $visit['visited_at'] . '<br>';
+        echo "<p>[" . $visit["domain"] . "] - " . $visit["visited_at"] . "</p>";
     }
 }
